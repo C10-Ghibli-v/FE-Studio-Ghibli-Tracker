ed6552f19587c4ef27370b24ccee8ee9d808856b
Laura Duque <duquelaura295@gmail.com>
2022-03-23 18:59:25 -0500

    ✨ create card movie component

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import CardMovie from "../components/CardMovie";
+import Header from "../components/Header";
 
 const MoviesList = () => {
   return (
     <div>
       <Header />
+      <CardMovie
+        title="Gumbal"
+        time={120}
+        year={2011}
+        stars="estrellas 5"
+        rottenTomatoes={100}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MoviesList
+export default MoviesList;
