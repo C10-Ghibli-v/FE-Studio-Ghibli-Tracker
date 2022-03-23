d730721301e3b4e641b93abf3cdb3f39dcd282f6
Enrique Henandez <enriquedev53@gmail.com>
2022-03-22 20:19:05 -0600

    Se agregaron estilos y responsive al componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import back from '../assets/icons/back.png'
 import search from '../assets/icons/search.png'
 
 const Header = () => {
-  return (
-    <nav className='p-3 w-screen'>
+  return ( 
+    <nav className='p-3 w-screen bg-slate-800'>
         <ul className='flex'>
             <li className='w-1/6 flex justify-start items-center'>
                 <a href="/"><img src={back} alt="back" className="w-5"/></a>
             </li>
             <li className='flex items-center justify-center w-4/6'>
-                <input className='w-6/7 border-2 mr-2 rounded-lg' type="text" placeholder="Search"/>
-                <img className="w-6 cursor-pointer" src={search} alt="search" />
+                <input className='w-6/7 px-2 rounded-md w-full placeholder-gray-400' type="text" placeholder="Search"/>
+                <img className="w-7 cursor-pointer pl-2" src={search} alt="search" />
             </li>
             <li className='w-1/6 flex justify-end'>
                 <a href="/"><img src={account} alt="perfil" className="w-7" /></a>
