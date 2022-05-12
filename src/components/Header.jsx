6ea443d1efc59858dd2e9032d1eb2928d6308c77
Laura Duque <duquelaura295@gmail.com>
2022-05-11 22:16:05 -0500

    get movie detail data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import back from '../assets/icons/back.png';
-import account from '../assets/icons/account.png';
+import back from "../assets/icons/back.png";
+import account from "../assets/icons/account.png";
 
 const Header = () => {
-    return(
-        <nav className='p-3 w-full bg-slate-800'>
-        <ul className='flex'>
-            <li className='w-1/6 flex justify-start items-center'>
-                <Link to="/MoviesList"><img src={back} alt="back" className="w-5"/></Link>
-            </li>
-            <li className='barra'>
-            </li>
-            <li className='flex items-right justify-end w-5/6'>
-                <a href="/"><img src={account} alt="perfil" className="w-7" /></a>
-            </li>
-        </ul>
+  return (
+    <nav className="p-3 w-full bg-slate-800">
+      <ul className="flex">
+        <li className="w-1/6 flex justify-start items-center">
+          <Link to="/MoviesList">
+            <img src={back} alt="back" className="w-5" />
+          </Link>
+        </li>
+        <li className="barra"></li>
+        <li className="flex items-right justify-end w-5/6">
+          <a href="/">
+            <img src={account} alt="perfil" className="w-7" />
+          </a>
+        </li>
+      </ul>
     </nav>
-);
-}
-  
-  export default Header
+  );
+};
+
+export default Header;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+
 const MovieDetail = () => {
-  return <h1>Movie Detail</h1>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState();
+
+  const getMovieDetail = async () => {
+    const apiUrl = "https://vast-chamber-00684.herokuapp.com/movies";
+    const response = await fetch(apiUrl);
+    const { data } = await response.json();
+    const detail = data.find((movie) => movie._id === id);
+    setMovie(detail);
+  };
+  console.log(movie);
+
+  useEffect(() => {
+    getMovieDetail();
+  }, []);
+
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default MovieDetail;
